88cf8e16e9b3577e8748ae0fc8203e9c0666497f
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-06-21 09:47:35 -0400

    Character class created

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Part One
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -15,3 +16,29 @@ const adventurer = {
     console.log(`${this.name} rolled a ${result}.`);
   },
 };
+
+//Part Two
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.roll();
+robin.companion.roll();
+robin.companion.companion.roll();
