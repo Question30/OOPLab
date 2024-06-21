d010a7b9ef3d53c2e8a5dcf3ea74b61984219020
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-06-21 09:40:20 -0400

    Adventure Object done

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ const adventurer = {
       type: "Flea",
     },
   },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
 };
